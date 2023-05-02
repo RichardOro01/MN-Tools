89e9005d67edfc3316ee62c1d9e343d079d84f0b
Richard Oropesa <richardoro2001@gmail.com>
2023-05-02 21:22:44 +0200

    feat: numeric optimization table

--- src/components/pages/NumericOptimization.tsx
+++ src/components/pages/NumericOptimization.tsx
@@ -1,9 +1,24 @@
 import { Button, Form, FormInstance, Input, Select, Table } from 'antd'
 import { ColumnsType } from 'antd/es/table'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import { fun } from '../utils';
+import { isRange } from 'mathjs';
+
+interface DataSource {
+    key: number;
+    it: number;
+    a: number;
+    b: number;
+    l: number;
+    x1: number;
+    x2: number;
+    y1: number;
+    y2: number
+}
 
 const NumericOptimization: React.FC = () => {
     const form = useRef<FormInstance<any>>(null);
+    const [dataSource, setDataSource] = useState<DataSource[]>([]);
     const columns: ColumnsType<any> | undefined = [
         {
             dataIndex: 'it',
@@ -49,7 +64,27 @@ const NumericOptimization: React.FC = () => {
 
     const handleBisection = () => {
         const {equa, interv_start, interv_end, point, tole} = form.current?.getFieldsValue();
+        const landa = tole * 0.1
+        let [a, b] = [Number(interv_start), Number(interv_end)]; 
+        const data: DataSource[] = [];
         console.log(equa, interv_start, interv_end, point, tole);
+        for (let it=0; b - a > Number(tole) && it<100; it++) {
+            const x1 = (( a + b ) / 2) - ( landa / 2 );
+            console.log(a,b,landa, (( a + b ) / 2), ( landa / 2 ))
+            const x2 = (( a + b ) / 2) + ( landa / 2 );
+            const y1 = fun(equa, x1);
+            const y2 = fun(equa, x2);
+            data.push({
+                key: it, l: b-a, ...{it, a, b, x1, x2, y1, y2}
+            })
+            if ( y1 < y2 ) {
+                b = x2;
+            } else {
+                a = x1;
+            }
+            console.log(it)
+        }
+        setDataSource(data);
     }
   return (
     <div className='flex flex-col items-center p-4'>
@@ -96,7 +131,10 @@ const NumericOptimization: React.FC = () => {
         </Form>
         <Button onClick={handleBisection}>Biseccion</Button>
         <Table
-            {...{columns}}
+            {...{columns, dataSource}}
+            pagination={false}
+            className='my-4'
+            scroll={{x: 500}}
         />
     </div>
   )
